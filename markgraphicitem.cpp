de426aaa78cd11c66280cba4d5b071e3ed76068d
Ariel <a.gawrys@ivm.pl>
2019-08-23 09:13:14 +0200

    Dopracowanie logiki klasy pola

--- markgraphicitem.cpp
+++ markgraphicitem.cpp
@@ -5,8 +5,12 @@
 
 MarkGraphicItem::MarkGraphicItem(QObject* parent):QGraphicsObject()
   ,_rect(0,0,128,128)
+  ,_isHover(false)
+  ,_isMakred(false)
+  ,_mark("")
+  ,_player(Item::Non)
 {
-
+    setAcceptHoverEvents(true);
 }
 
 QRectF MarkGraphicItem::boundingRect() const
@@ -20,24 +24,68 @@ void MarkGraphicItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *o
 
     painter->save();
     QPen pen = painter->pen();
-    pen.setColor(QColor("#FF0000"));
-
+    QTextOption texOpt(Qt::AlignCenter);
+    QFont font;
+    font.setPointSize(32);
+    pen.setColor(QColor("#000"));
     painter->setPen(pen);
+    painter->setFont(font);
+
+    if(_isHover and !_isMakred) {
+        pen.setColor(QColor("#FF0000"));
+        painter->setPen(pen);
+        QString pom  = getActualMark();
+        painter->drawText(_rect,pom,texOpt);
+    } else {
+        painter->drawText(_rect,_mark,texOpt);
+    }
 
     painter->drawRect(_rect);
-    painter->drawText(_rect,"X");
-
     painter->restore();
 
+}
+
+void MarkGraphicItem::setPlayer(const Item item)
+{
+    _player = item;
+}
+
+void MarkGraphicItem::reset()
+{
+    _isMakred = false;
+    _mark = "";
+    update();
 }
 
 void MarkGraphicItem::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
 {
     qDebug()<<__PRETTY_FUNCTION__;
-
+    _isHover = true;
+    update();
+    QGraphicsObject::hoverEnterEvent(event);
 }
 
 void MarkGraphicItem::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
 {
     qDebug()<<__PRETTY_FUNCTION__;
+    _isHover = false;
+    update();
+    QGraphicsObject::hoverLeaveEvent(event);
+}
+
+void MarkGraphicItem::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    _isMakred = true;
+    _mark = getActualMark();
+    update();
+    qDebug()<<__PRETTY_FUNCTION__;
+}
+
+QString MarkGraphicItem::getActualMark()
+{
+    switch (_player) {
+    case Item::Crosses: return "X";
+    case Item::Noughts: return "O";
+    default: return "";
+    }
 }
--- markgraphicitem.h
+++ markgraphicitem.h
@@ -4,6 +4,7 @@
 #include <QGraphicsObject>
 #include <QObject>
 #include <QRectF>
+#include "item.h"
 
 class MarkGraphicItem : public QGraphicsObject
 {
@@ -14,13 +15,25 @@ public:
 public:
     QRectF boundingRect() const override;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+    void setPlayer(const Item item);
+    void reset();
 
 protected:
     void hoverEnterEvent(QGraphicsSceneHoverEvent *event) override;
     void hoverLeaveEvent(QGraphicsSceneHoverEvent *event) override;
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
 
 private:
+    QString getActualMark();
+
     QRectF _rect;
+    bool _isHover;
+    bool _isMakred;
+    QString _mark;
+    Item _player; /**< Actual player*/
+
+    // QGraphicsItem interface
+protected:
 };
 
 #endif // MARKGRAPHICITEM_H
